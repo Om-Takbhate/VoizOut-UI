52aef3b07ea7a595d814e7b010dc2589b1121184
Om Takbhate <omtakbhate007@gmail.com>
2025-06-21 09:49:25 +0530

    refactor: removing the vite setting

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',   // Crucial for allowing access
-    port: 5173,        // Default Vite port
-    strictPort: true
-  }
+  plugins: [react(), tailwindcss()]
 })
